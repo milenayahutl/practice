8646b6b01b50fdaf38937a2632dcb3f3478ba74b
milena.yakhutl.i@mail.ru <milena.yakhutl.i@mail.ru>
2025-02-28 12:40:56 +0300

    add sfml; test push

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include "todo.h"
 
 int main(int, char**) {
+
+    //создание окна
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "ToDo List with SFML")
+    //шрифт для текста
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+        std::cerr << "Error loading font!" << std::endl;
+        return -1;
+    }
+
+    //текстовые штуки
+    sf::Text title("ToDo List", font, 40);
+    title.setPosition(50, 20);
+    title.setFillColor(sf::Color::White);
+
+    sf::Text addNoteText("Add Note:", font, 24);
+    addNoteText.setPosition(50, 100);
+    addNoteText.setFillColor(sf::Color::White);
+
+    sf::Text showNotesText("Show Notes", font, 24);
+    showNotesText.setPosition(50, 150);
+    showNotesText.setFillColor(sf::Color::White);
+
+    sf::Text deleteNoteText("Delete Note", font, 24);
+    deleteNoteText.setPosition(50, 200);
+    deleteNoteText.setFillColor(sf::Color::White);
+
+    sf::Text clearListText("Clear List", font, 24);
+    clearListText.setPosition(50, 250);
+    clearListText.setFillColor(sf::Color::White);
+
+    //поле ввода текста
+
+    
+
+
+
     TodoList list;
     char newNote[maxNoteLength];
     int index = 0, choice = 0;
--- todo.h
+++ todo.h
@@ -5,6 +5,8 @@
 
 const int maxNotesNumber = 1000;
 const int maxNoteLength = 1000;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
 struct TodoNote{
     char text[maxNoteLength];
